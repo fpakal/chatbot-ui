305895e54220ebc1e58070ea97f0ad585023b3e3
fpakal <129833941+fpakal@users.noreply.github.com>
2023-06-03 19:41:57 -0400

    Update _document.tsx
    MTA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,12 @@ export default function Document(props: Props) {
   return (
     <Html lang={currentLocale}>
       <Head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
-      </Head>
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-title" content="ChatGPT 1"></meta>
+  <title>CHATGPT1</title>
+  <meta name="description" content="Experience the power of AI with our advanced chatbot. Leveraging the capabilities of GPT-3, our chatbot provides accurate responses and includes a search function for quick information retrieval. Ideal for anyone looking to interact with cutting-edge AI technology.">
+</Head>
+
       <body>
         <Main />
         <NextScript />
